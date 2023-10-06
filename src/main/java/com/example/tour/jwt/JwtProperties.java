7a71345db58fe37684f286822e5611d230e88923
Hyeong Jun Noh <munhwas1140@naver.com>
2023-10-06 16:17:04 +0900

    d

--- src/main/java/com/example/tour/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.example.tour.jwt;
-
-public interface JwtProperties {
-    String SECRET = "vgbqny123123123aszxcbqr8114553tqblma3416341531kadmklvaasdadflvkm23";
-    int EXPIRATION_TIME =  864000000; // 10일, ms 단위이다.
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
