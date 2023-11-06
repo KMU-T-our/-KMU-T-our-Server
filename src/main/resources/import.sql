c0cb7f7ff8e60b40a4d22a38f0d8c63ad53a781a
Hyeong Jun Noh <munhwas1140@naver.com>
2023-11-06 15:29:04 +0900

    [dev] add temp user in import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (social_id, type, name)
+VALUES ('11111', 'kakao', 'user1');
+INSERT INTO user (social_id, type, name)
+VALUES ('22222', 'kakao', 'user2');
+INSERT INTO user (social_id, type, name)
+VALUES ('33333', 'kakao', 'user3');
