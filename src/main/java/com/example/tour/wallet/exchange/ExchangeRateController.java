baa8e89f332fa77d551fb6b8dbddf9bdb0ac1bde
Hyeong Jun Noh <munhwas1140@naver.com>
2023-11-07 16:22:15 +0900

    [refactor] Add exchangeRateService resultCacheMap

--- src/main/java/com/example/tour/wallet/exchange/ExchangeRateController.java
+++ src/main/java/com/example/tour/wallet/exchange/ExchangeRateController.java
@@ -1,6 +1,7 @@
 package com.example.tour.wallet.exchange;
 
 import com.example.tour.wallet.exchange.dto.ExchangeRateResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class ExchangeRateController {
-    ExchangeRateService exchangeRateService = new ExchangeRateService();
+    ExchangeRateService exchangeRateService;
+
     @GetMapping("/api/exchange")
     public List<ExchangeRateResponse> callExchangeApi(
             @RequestParam(value="searchDate", required = false) String searchDate
