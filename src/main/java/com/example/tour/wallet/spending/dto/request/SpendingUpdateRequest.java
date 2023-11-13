873f479b4dfba3c2e6c06855d53fec2430d8e907
decollzoq <decollzoq@gmail.com>
2023-11-13 23:22:44 +0900

    [Refactor] 변수 이름과 spendingDate의 데이터 타입 변경

--- src/main/java/com/example/tour/wallet/spending/dto/request/SpendingUpdateRequest.java
+++ src/main/java/com/example/tour/wallet/spending/dto/request/SpendingUpdateRequest.java
@@ -2,14 +2,22 @@ package com.example.tour.wallet.spending.dto.request;
 
 import com.example.tour.wallet.spending.domain.Spending;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class SpendingUpdateRequest {
 
-    private long id;
-    private String title;
-    private Spending.SpendingTag tag;
-    private long amount;
-    private String date;
+    private long spendingId;
+    private String spendingTitle;
+    private Spending.SpendingTag spendingTag;
+    private long spendingAmount;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate spendingDate;
 
 }
