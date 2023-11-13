3b8d215e1d51c47f185cc6d061df38b27cd69644
decollzoq <decollzoq@gmail.com>
2023-11-13 23:13:33 +0900

    [Refactor] GET 함수 이름 변경, POST 함수 return 값 변경

--- src/main/java/com/example/tour/wallet/spending/SpendingController.java
+++ src/main/java/com/example/tour/wallet/spending/SpendingController.java
@@ -4,10 +4,14 @@ import com.example.tour.wallet.spending.domain.Spending;
 import com.example.tour.wallet.spending.dto.request.SpendingCreateRequest;
 import com.example.tour.wallet.spending.dto.request.SpendingUpdateRequest;
 import com.example.tour.wallet.spending.dto.response.SpendingResponse;
+import com.example.tour.wallet.spending.dto.response.SpendingSaveResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -16,23 +20,26 @@ public class SpendingController {
     private final SpendingService spendingService;
 
     @PostMapping("/api/wallet/spending")
-    public void saveWalletSpending(@RequestBody SpendingCreateRequest request){
-        spendingService.saveSpending(request);
+    public SpendingSaveResponse saveSpending(@RequestBody SpendingCreateRequest request){
+        return spendingService.saveSpending(request);
     }
 
-    @GetMapping("/api/wallet/spending/entire")
-    public List<SpendingResponse> getWalletEntireSpending(@RequestParam Long projectUserId){
-        return spendingService.getEntireSpending(projectUserId);
+    @GetMapping("/api/wallet/spending/all")
+    public List<SpendingResponse> getSpendingAll(@RequestParam Long projectUserId){
+        return spendingService.getSpendingAll(projectUserId);
     }
 
     @GetMapping("/api/wallet/spending/tag")
-    public List<SpendingResponse> getWalletFilteringTagSpending(@RequestParam Long projectUserId, Spending.SpendingTag tag){
-        return spendingService.getFilteringTagSpending(projectUserId, tag);
+    public List<SpendingResponse> getSpendingFilteringByTag(@RequestParam Long projectUserId, @RequestParam List<String> tags){
+        List<Spending.SpendingTag> spendingTags = tags.stream()
+                .map(Spending.SpendingTag::valueOf)
+                .collect(Collectors.toList());
+        return spendingService.getFilteringByTag(projectUserId, spendingTags);
     }
 
     @GetMapping("/api/wallet/spending/date")
-    public List<SpendingResponse> getWalletFilteringDateSpending(@RequestParam Long projectUserId, String date){
-        return spendingService.getFilteringDateSpending(projectUserId, date);
+    public List<SpendingResponse> getSpendingFilteringByDate(@RequestParam Long projectUserId, @DateTimeFormat(pattern = "YYYY-MM-DD") @RequestParam LocalDate date){
+        return spendingService.getFilteringByDate(projectUserId, date);
     }
 
     @PutMapping("/api/wallet/spending")
