dd54be70e3b6dfd7b5bbd6a076f7a51b244f52a1
Hyeong Jun Noh <munhwas1140@naver.com>
2023-11-16 00:02:18 +0900

    [Feat] 배포 자동화 테스트

--- start.sh
+++ start.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+ROOT_PATH="/home/ubuntu/t_our"
+JAR="$ROOT_PATH/application.jar"
+
+APP_LOG="$ROOT_PATH/application.log"
+ERROR_LOG="$ROOT_PATH/error.log"
+START_LOG="$ROOT_PATH/start.log"
+
+NOW=$(date +%c)
+
+echo "[$NOW] $JAR 복사" >> $START_LOG
+cp $ROOT_PATH/build/libs/tour-0.0.1-SNAPSHOT.jar $JAR
+
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup noup java -jar /home/ubuntu/t_our/tour-0.0.1-SNAPSHOT.jar --spring.profiles.active=h2 &
+nohup java -jar $JAR --spring.profiles.active=h2 > $APP_LOG 2> $ERROR_LOG &
+
+SERVICE_PID=$(pgrep -f $JAR)
+echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
