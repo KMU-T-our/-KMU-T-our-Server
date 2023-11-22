309c0cdacc2a8e28d476ecfd2681964937d3ea7a
Hyeong Jun Noh <munhwas1140@naver.com>
2023-11-23 08:52:27 +0900

    [Refactor] 프로젝트 업데이트 DTO, 스케줄 업데이트 DTO NoArgsContstructor 어노테이션 추가

--- src/main/java/com/example/tour/project/dto/ProjectUpdateDTO.java
+++ src/main/java/com/example/tour/project/dto/ProjectUpdateDTO.java
@@ -1,9 +1,13 @@
 package com.example.tour.project.dto;
 
 import com.example.tour.project.Project;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class ProjectUpdateDTO {
     public Long projectId;
     public String name;
--- src/main/java/com/example/tour/tags/schedule/dto/ScheduleTagUpdateRequest.java
+++ src/main/java/com/example/tour/tags/schedule/dto/ScheduleTagUpdateRequest.java
@@ -1,10 +1,12 @@
 package com.example.tour.tags.schedule.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ScheduleTagUpdateRequest {
     private Long tagId;
     private String name;
