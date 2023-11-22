d18bbefa14c5e66bae142c5ee5943c5d18a38dff
decollzoq <decollzoq@gmail.com>
2023-11-22 19:49:18 +0900

    [Refactor] 식당 태그 생성, 수정 시 response 추가

--- src/main/java/com/example/tour/tags/restaurant/domain/RestaurantTag.java
+++ src/main/java/com/example/tour/tags/restaurant/domain/RestaurantTag.java
@@ -5,15 +5,10 @@ import com.example.tour.project.Project;
 import com.example.tour.tags.restaurant.dto.RestaurantTagCreateRequest;
 import com.example.tour.tags.restaurant.dto.RestaurantTagUpdateRequest;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import java.sql.Date;
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
